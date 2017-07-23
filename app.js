93a9dfb350de8dfd76b5199493114f7bd7a4cbf5
rushdi <rushdi@gmail.com>
2017-07-23 12:22:43 +0200

    server file added

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+app.set('view engine',"ejs");
+
+app.get('/',function(req,res){
+	res.render("home");
+	});
+
+app.get('/contact',function(req,res){
+	res.render("contact");
+	});
+
+
+app.get('/profile/:name',function(req,res){
+
+	//dummy  data
+	var data = {age :25,job:'ninja',hobbies:['eating','coding','gaming']};
+	res.render('profile',{person:req.params.name,data:data});
+	});
+
+
+
+
+app.listen(3000);
